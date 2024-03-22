0061ac11f128460cc4e4f49cf8a535d5710dfaf7
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-03-22 17:01:34 +0200

    Added logs to the service layer

--- src/main/java/com/example/buildingservice/service/impl/BuildingServiceImpl.java
+++ src/main/java/com/example/buildingservice/service/impl/BuildingServiceImpl.java
@@ -38,52 +38,72 @@ public class BuildingServiceImpl implements BuildingService {
     private final BuildingMapperForInformationPage buildingMapperForInformationPage;
     @Override
     public Page<BuildingDtoForViewAll> getAllForAdmin(Integer page, Integer pageSize, String houseName) {
+        log.info("BuildingServiceImpl-getAllForAdmin start");
         Pageable pageable = PageRequest.of(page, pageSize, Sort.by(Sort.Order.desc("id")));
-        return buildingMapperForViewAll.toDtoPage(buildingRepository.findAllByNameLike(houseName, pageable));
+        Page<BuildingDtoForViewAll> result = buildingMapperForViewAll.toDtoPage(buildingRepository.findAllByNameLike(houseName, pageable));
+        log.info("BuildingServiceImpl-getAllForAdmin finish");
+        return result;
     }
     @Override
     public Page<BuildingDtoForViewAll> getAllForCustomer(BuildingDtoForFilter buildingDtoForFilter) {
+        log.info("BuildingServiceImpl-getAllForCustomer start");
         Specification<Building> specification = new BuildingSpecification(buildingDtoForFilter);
         Pageable pageable = PageRequest.of(buildingDtoForFilter.getPage(), buildingDtoForFilter.getPageSize(), Sort.by(Sort.Order.desc("id")));
         Page<BuildingDtoForViewAll> result =  buildingMapperForViewAll.toDtoPage(buildingRepository.findAll(specification, pageable));
+        log.info("BuildingServiceImpl-getAllForCustomer finish");
         return result;
     }
     @Override
     public Building getById(Long builderId) {
-        return buildingRepository.findById(builderId).orElseThrow(
+        log.info("BuildingServiceImpl-getById start");
+        Building building =  buildingRepository.findById(builderId).orElseThrow(
                 ()-> {
                     log.error("Building with id={} not found", builderId);
                     return new EntityNotFoundException("Building with id="+builderId+" not found");
                 }
-        );    }
+        );
+        log.info("BuildingServiceImpl-getById finish");
+        return building;
+    }
     @Override
     public Map<Long, String> getAllCorpsByBuildingId(Long builderId) {
+        log.info("BuildingServiceImpl-getAllCorpsByBuildingId start");
         Building building = getById(builderId);
         Map<Long, String> corpses = new HashMap<>();
         for (Corps corps : building.getCorps()) {
             corpses.put(corps.getId(), corps.getName());
         }
+        log.info("BuildingServiceImpl-getAllCorpsByBuildingId finish");
         return corpses;
     }
     @Override
     @Transactional
     public void changeStatus(Long builderId, StatusState statusState) {
+        log.info("BuildingServiceImpl-changeStatus start");
         Building building = getById(builderId);
         building.setStatusState(statusState);
         save(building);
+        log.info("BuildingServiceImpl-changeStatus finish");
     }
     @Override
     @Transactional
     public void save(Building building){
+        log.info("BuildingServiceImpl-save start");
         buildingRepository.save(building);
+        log.info("BuildingServiceImpl-save finish");
     }
     @Override
     public BuildingDtoForInformationPage getBuildingForInformationPage(Long buildingId){
-        return buildingMapperForInformationPage.toDto(getById(buildingId));
+        log.info("BuildingServiceImpl-getBuildingForInformationPage start");
+        BuildingDtoForInformationPage buildingDtoForInformationPage = buildingMapperForInformationPage.toDto(getById(buildingId));
+        log.info("BuildingServiceImpl-getBuildingForInformationPage finish");
+        return buildingDtoForInformationPage;
     }
     @Override
     @Transactional
     public void add(BuildingDtoForAdd buildingDtoForAdd) throws IOException {
+        log.info("BuildingServiceImpl-add start");
         save(buildingMapperForAdd.updateEntityFromDto(buildingDtoForAdd, this));
+        log.info("BuildingServiceImpl-add finish");
     }
 }
--- src/main/java/com/example/buildingservice/service/impl/CorpsServiceImpl.java
+++ src/main/java/com/example/buildingservice/service/impl/CorpsServiceImpl.java
@@ -5,19 +5,23 @@ import com.example.buildingservice.repository.CorpsRepository;
 import com.example.buildingservice.service.CorpsService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class CorpsServiceImpl implements CorpsService {
     private final CorpsRepository corpsRepository;
-
     @Override
     public Corps getById(Long corpID) {
-        return corpsRepository.findById(corpID).orElseThrow(
+        log.info("CorpsServiceImpl-getById start");
+        Corps corps = corpsRepository.findById(corpID).orElseThrow(
                 ()-> {
                     throw new EntityNotFoundException("Corps with id="+corpID+" not found");
                 }
         );
+        log.info("CorpsServiceImpl-getById finish");
+        return corps;
     }
 }
